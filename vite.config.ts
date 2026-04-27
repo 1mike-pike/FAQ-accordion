e7309b99caf31822db93e2c76d3fd7d6b622be62
1mike-pike <166156379+1mike-pike@users.noreply.github.com>
2026-04-27 16:41:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
     plugins: [react(), tailwindcss()],
-    base: command === 'build' ? '/FAQ-accordion/' : '/',
+    base: '/FAQ-accordion/',
 }))
